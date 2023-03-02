98c626a0b859805d705a5a13042ec1d8aa8fb84e
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-02 20:05:40 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard', 'eslint-config-prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
