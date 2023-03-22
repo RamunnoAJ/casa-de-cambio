47f34d432791df19955099f23cee39a616bd1dd3
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-22 00:01:44 -0300

    setup cypress environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['standard', 'eslint-config-prettier'],
+  extends: ['standard', 'eslint-config-prettier', 'plugin:cypress/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
